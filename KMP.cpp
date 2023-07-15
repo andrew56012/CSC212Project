55e5c39071a4d6c92b17dc0781f2f7bb597c1382
AML0424 <98441522+AML0424@users.noreply.github.com>
2023-07-14 23:16:56 -0400

    Creating the starting point
    I created a few header files along with functions I expect us to need. I am considering combining the two header files however for now I believe having them separated will work. I have also started on the main function

---
+++ KMP.cpp
@@ -0,0 +1,18 @@
+#include "KMP.h"
+
+KMP::KMP(std::string base){
+
+}
+KMP::~KMP(){
+
+}
+
+int KMP::findIndex(std::string find){
+
+}
+int KMP::skipTo(std::string pattern){
+
+}
+int KMP::get_efficiency(){
+    return this-> Runs;
+}
---
+++ boyerMoore.cpp
@@ -0,0 +1,18 @@
+#include "boyerMoore.h"
+
+boyerMoore::boyerMoore(std::string base){
+
+}
+boyerMoore::~boyerMoore(){
+
+}
+
+int boyerMoore::findIndex(std::string find){
+
+}
+int boyerMoore::compare(std::string pattern){
+
+}
+int boyerMoore::get_efficiency(){
+    return this->Runs;
+}
