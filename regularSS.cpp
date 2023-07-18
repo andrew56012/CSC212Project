a8e397f93bf2059dce567c35fcea639b0b6fcf77
tanyavm7 <90417982+tanyavm7@users.noreply.github.com>
2023-07-18 18:14:04 -0400

    Update regularSS.cpp

--- regularSS.cpp
+++ regularSS.cpp
@@ -1,11 +1,28 @@
 #include "regularSS.h"
 
-KMP::KMP(std::string given){
+std::vector<int> StringSearch::search(const std::string& text, const std::string& pattern) {
+    std::vector<int> index;
 
-    this -> base = given;
+    if (pattern.empty()) {
+        // Pattern is empty, return an empty result
+        return index;
+    }
 
+    int textL = text.length();
+    int patternL = pattern.length();
+
+    for (int i = 0; i <= textL - patternL; ++i) {
+        int j;
+        for (j = 0; j < patternL; ++j) {
+            if (text[i + j] != pattern[j]) {
+                break;
+            }
+        }
+
+        if (j == patternL) {
+            index.push_back(i);
+        }
+    }
+
+    return index;
 }
-KMP::~KMP(){
-
-}
-regularSS::regularSS(std::s
