526829d079d2998b301e21da1e19ce96cebb4b34
andrew56012 <98777690+andrew56012@users.noreply.github.com>
2023-07-18 14:49:19 -0400

    Update boyerMoore.cpp

--- boyerMoore.cpp
+++ boyerMoore.cpp
@@ -13,6 +13,12 @@ std::vector<int> boyerMoore::findIndex(std::string find){
     int BadCharacter[256];
     BadCharacterHeur(pat, PatLength, BadCharacter);
 
+    int Pos[PatLength + 1];
+    int shiftArr[Patlength+1];
+    for(int i=0){
+        shiftArr[i] = 0;
+    }
+
 }
 int boyerMoore::compare(std::string pattern){
 
