3e78353113013f9de8911d959650bbdbd59ac7d9
andrew56012 <98777690+andrew56012@users.noreply.github.com>
2023-07-19 13:32:42 -0400

    Update boyerMoore.cpp

--- boyerMoore.cpp
+++ boyerMoore.cpp
@@ -7,6 +7,37 @@ boyerMoore::~boyerMoore(){
 
 }
 
+void strongersuffix(int *shift, int *borderpos, char *pattern, int alength)
+{
+	int i=alength;
+	int j=alength+1;
+	borderpos[i]=j;
+	while(i>0)
+	{
+		while(j<=a && pattern[i-1] != pattern[j-1])
+		{
+			if (shift[j]==0){
+				shift[j] = j-i;
+            }
+			j = borderpos[j];
+		}
+		i--;
+        j--;
+		bpos[i] = j;
+	}
+}
+void weaker suffix(int *shift, int *bpos, char *pattern, int alength)
+{
+	int j = borderpos[0];
+	for(int i=0; i<=alength; i++)
+	{
+		if(shift[i]==0)
+			shift[i] = j;
+		if (i==j)
+			j = bpos[j];
+	}
+}
+
 std::vector<int> boyerMoore::findIndex(std::string find){
     int PatLength = pat.size();
     int TxtLength = txt.size();
