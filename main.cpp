dcb78a4cf2eb645d4a870455f1ab2bfc21ab2adc
SeanRCSC <112998933+SeanRCSC@users.noreply.github.com>
2023-07-23 23:30:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -68,39 +68,45 @@ int main(){
     }
     std::cout << "\n";
     //have to only perform this once to ensure number of runs for efficiency is correct
-    std::vector<int> bmIndex = BM.findIndex(find);
+    std::vector<int> bmIndex = BM.findIndex(base,find);
 
     //state indexes found
     if(bmIndex.empty() == 1) {
         std::cout << "The given pattern could not be found within the base string using the Boyer-Moore algorithm.\n";
         return 0;
     }
-    else if(kIndex.size() == 1){
-        std::cout << "Using the Boyer-Moore algorithm it is found that the pattern appears at index " << kIndex[0]<< ".\n";
-        std::cout << "This program looped a total of " << boyerMoore.get_loops() << " times to find this.\n";
-        std::cout << "This algorithm took " << boyerMoore.get_runTime() << " seconds to run.\n";
+    else if(bmIndex.size() == 1){
+        std::cout << "Using the Boyer-Moore algorithm it is found that the pattern appears at index " << bmIndex[0]<< ".\n";
+        std::cout << "This program looped a total of " << BM.get_efficiency() << " times to find this.\n";
+        std::cout << "This algorithm took " << BM.get_runTime() << " seconds to run.\n";
     }
     else{
         std::cout << "Using the Boyer-Moore algorithm it is found that the pattern appears at indexes ";
         //print all indexes where the pattern was found
-        for(int i = 0; i < kIndex.size(); i++){
-            std::cout << kIndex[i];
-            if(i == kIndex.size()-1){
+        for(int i = 0; i < bmIndex.size(); i++){
+            std::cout << bmIndex[i];
+            if(i == bmIndex.size()-1){
                 std::cout << ".\n";
             }
             else{
                 std::cout << " ";
             }
         }
-        std::cout << "This program ran a total of " << boyerMoore.get_loops() << " times to find this.\n";
-        std::cout << "This algorithm took " << boyerMoore.get_runTime() << " seconds to run.\n";
+        std::cout << "This program ran a total of " << BM.get_efficiency() << " times to find this.\n";
+        std::cout << "This algorithm took " << BM.get_runTime() << " seconds to run.\n";
     }
     std::cout << "\n";
 
     if(RSS == 'Y'){
 
         regularSS BFM;
+        std::cout << "Using the Brute Force method, it is found that the pattern appears at indexes ";
+
         std::vector<int> bruteForce = BFM.search(base,find);
+        for (int i=0;i<bruteForce.size();i++){
+            std::cout<<bruteForce[i]<<" ";
+        }
+        std::cout<<std::endl;
         std::cout << "For comparison a string search using the brute force method would find these indexes in " << BFM.getLoopCount() << " loops and took " << BFM.getExecutionTime() << " seconds to run.\n";
 
 
