a2a7b7c565c77ad4add50995c998fc5754fb4d8f
SeanRCSC <112998933+SeanRCSC@users.noreply.github.com>
2023-07-24 14:59:39 -0400

    Update boyerMoore.cpp

--- boyerMoore.cpp
+++ boyerMoore.cpp
@@ -8,117 +8,161 @@ boyerMoore::~boyerMoore(){
 
 }
 
-void boyerMoore::GoodSuffixShiftCase1(std::vector<int> shift, std::vector<int> bpos, std::string pattern){ //function to compute stronger good suffix
-    int patSize = pattern.size();    //the shift and borderPos is an array with size equal to the pattern + 1 
+//function to compute stronger good suffix
+void boyerMoore::GoodSuffixShiftCase1(std::vector<int> shift, std::vector<int> bpos, std::string pattern){
+
+    //the shift and borderPos is an array with size equal to the pattern + 1
+    int patSize = pattern.size();
     int tempstrSize = patSize;
     int j = patSize + 1;
 
-    bpos[patSize] = j; //Set borderPos[patternLength] to patternLength+1.
+    //Set borderPos[patternLength] to patternLength+1.
+    bpos[patSize] = j;
     while(tempstrSize > 0) {
-        while(j <= patSize && pattern[tempstrSize - 1] != pattern[j -1 ]){    //if the spot at the pattern does not equal the pattern
-            if (shift[j]==0){   //if shift is 0
-                shift[j] = j-tempstrSize;    //shift will be j-temp
+        //if the spot at the pattern does not equal the pattern
+        while(j <= patSize && pattern[tempstrSize - 1] != pattern[j -1 ]){
+            //if shift is 0
+            if (shift[j]==0){
+                //shift will be j-temp
+                shift[j] = j-tempstrSize;
             }
             j = bpos[j];
+            //count each time loop runs
+            preRuns++;
         }
         tempstrSize--;
         j--;
         bpos[tempstrSize] = j;
-        this->Runs++;
+        //count each time loop runs
+        preRuns++;
     }
 }
 
-void boyerMoore::GoodSuffixShiftCase2(std::vector<int> shift, std::vector<int> bposArr, std::string pattern){ //function to compute weaker good suffix
-    int patSize = pattern.size();
-    int tempstrSize = patSize;
+//function to compute weaker good suffix
+void boyerMoore::GoodSuffixShiftCase2(std::vector<int> shift, std::vector<int> bposArr, std::string pattern){
     int patSize = pattern.size();
     int bpos = bposArr[0];
     for(int i=0; i<=patSize; i++){
-        if(shift[i]==0){ //shift is not set to anything
-            shift[i] = bpos; //shift is the border position at this location
+        //shift is not set to anything
+        if(shift[i]==0){
+            //shift is the border position at this location
+            shift[i] = bpos;
         }
-        if (i == bpos){    //when I is equal to the border position
-            bpos=bposArr[bpos];    //update the border position
+        //when i is equal to the border position
+        if (i == bpos){
+            //update the border position
+            bpos=bposArr[bpos];
         }
-        this->Runs++;
+        //count each time loop runs
+        this->preRuns++;
     }
 }
 
-
-void boyerMoore::BadCharacterHeur(int BadCharacters[],std::string pat){ //function to compute bad suffix
-    int patSize = pattern.size();
-    int tempstrSize = patSize;
-
+//function used to preprocess the pattern into a bad characters table
+void boyerMoore::BadCharacterHeur(int BadCharacters[],std::string pat){
+    // calculate size of the pattern we are searching for
     int patSize=pat.size();
 
+    //Loop sets all elements of the BadCharacters Array to -1
+    //Initialized to -1 to show that no characters in the patter have been encountered
     for (int i=0; i<256;i++){
-        BadCharacters[i]=-1;    //initialize elements
-        this->Runs++;
+        BadCharacters[i]=-1;
+        //count each time loop runs
+        this->preRuns++;
     }
 
+    //Loop fills BadCharacters array with rightmost index of each letter in pattern
     for (int i=0; i<patSize;i++){
-        BadCharacters[pat[i]]=i;    //record rightmost occurance of each character in pattern
-        this->Runs++;
+        BadCharacters[pat[i]]=i;
+        //count each time loop runs
+        this->preRuns++;
     }
 }
 
-std::vector<int> boyerMoore::findIndex(const std::string& text, const std::string& pat) { //implementation of both heuristics
+//implementation of both heuristics
+std::vector<int> boyerMoore::findIndex(const std::string& text, const std::string& pat) {
+    //start runtime of algorithm
     std::chrono::time_point<std::chrono::system_clock> begin,end;
     begin = std::chrono::system_clock::now();
     int patSize = pat.size();
     int strSize = text.size();
+    //size of all extended ascii characters
     int BadCharacters[256];
+    //preprocess bad characters in the patters
     BadCharacterHeur(BadCharacters, pat);
 
-    std::vector<int> bpos(patSize + 1, 0);        //array to store boarder positions
-    std::vector<int> shiftArr(patSize + 1, 0);    //array to store shift
+    //vectors to store boarder positions and shifts
+    std::vector<int> bpos(patSize + 1, 0);
+    std::vector<int> shiftArr(patSize + 1, 0);
 
-    GoodSuffixShiftCase1(shiftArr, bpos, pat);    //compute stronger good suffix
-    GoodSuffixShiftCase2(shiftArr, bpos, pat);    //compute weaker good suffix
+    //preprocess stronger and weaker suffixes.
+    GoodSuffixShiftCase1(shiftArr, bpos, pat);
+    GoodSuffixShiftCase2(shiftArr, bpos, pat);
 
-    std::vector<int> region; //List of indexes where the pattern appears
-    bool found = false;     //Used to keep track if at least one match has been found
+    //List of indexes where the pattern appears
+    std::vector<int> indexes;
+
+    //Used to keep track if at least one match in string and pattern has been found.
+    bool found = false;
     int shift = 0;
 
-    while (shift <= (strSize - patSize)) { //main pattern searching loop
+    //search for patterns in string
+    while (shift <= (strSize - patSize)) {
         int patIndex = patSize - 1;
         while (patIndex >= 0 && pat[patIndex] == text[shift + patIndex]) {
             patIndex--;
+            //count each time loop runs
             this->Runs++;
         }
-        if (patIndex < 0) {   //pattern found at current position
-            region.push_back(shift);    
+
+        //pattern found at current position
+        if (patIndex < 0) {
+            //pattern has been found
             found = true;
+            //add index to vector
+            indexes.push_back(shift);
+            //temp var for shift
             int tempBadCharacterShift = 1;
-            if (shift + patSize < strSize) {    //calculate bad character shift using characters after matched pattern
+            //calculate bad character shift using characters after matched pattern
+            if (shift + patSize < strSize) {
                 tempBadCharacterShift = patSize - BadCharacters[text[shift + patSize]];
             }
             shift += std::max(shiftArr[0], tempBadCharacterShift);
-        } else {    //pattern mismatch so use both good and bad heuristic to determine shift
+        }
+        //pattern mismatch so use both good and bad heuristic to determine shift
+        else {
+            //take the greater of the two and add to the shift
             shift += std::max(shiftArr[patIndex + 1], patIndex - BadCharacters[text[shift + patIndex]]);
         }
         this->Runs++;
     }
-
+    //if there is at least one instance of the pattern found, returns indexes they were found at
     if (found) {
-        end = std::chrono::system_clock::now();
-        this->runTime = end-begin; //caluclate run time
-        return region; //return list of occurances of the pattern in the text
-    } else {
+        //end runtime
         end = std::chrono::system_clock::now();
         this->runTime = end-begin;
-        return {}; //return empty vector if pattern isn't found
+        return indexes;
     }
-
+    //else no patterns found and empty vector returned.
+    else {
+        //end runtime
+        end = std::chrono::system_clock::now();
+        this->runTime = end-begin;
+        return {};
+    }
 }
 
-int boyerMoore::get_efficiency(){  //get number of runs performed for efficiency
+//returns amount of loops from just searching through string for pattern
+int boyerMoore::get_Searchefficiency() {
     return this->Runs;
 }
-double boyerMoore::get_runTime(){  //get run time of pattern search
-    return this-> runTime.count();
+
+//returns amount of loops from good suffix and bad character preprocessing heuristics
+int boyerMoore::get_Processefficiency() {
+    return this->preRuns;
 }
 
-
-
-//we all worked on a shared IDE for the final commits to each file 
+//returns runtime of algorithm
+double boyerMoore::get_runTime(){
+    return this-> runTime.count();
+}
