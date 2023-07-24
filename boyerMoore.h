8c1af8f6c40a89ada4753c43fb5ed485fe8cbf12
SeanRCSC <112998933+SeanRCSC@users.noreply.github.com>
2023-07-24 14:59:59 -0400

    Update boyerMoore.h

--- boyerMoore.h
+++ boyerMoore.h
@@ -8,6 +8,7 @@ class boyerMoore{
 
 private:
     int Runs=0;
+    int preRuns=0;
     std::chrono::duration<double> runTime;
     std::string base;
 
@@ -18,16 +19,19 @@ public:
     ~boyerMoore();
 
     //Used to look for pattern in base through indexes
-    //returns amount of time it took to find the index by tracking runs
-    int get_efficiency();
+    //returns amount of loops it took to find the index by tracking runs
+    int get_Searchefficiency();
+    //returns amount of loops taken during preprocessing
+    int get_Processefficiency();
     //Bad character heuristic approach function
     void BadCharacterHeur(int BadCharacters[],std::string pat);
     //Stronger Good heuristic approach function
     void GoodSuffixShiftCase1(std::vector<int> shift, std::vector<int> bpos, std::string pattern);
     //Weaker Good heuristic approach function
     void GoodSuffixShiftCase2(std::vector<int> shift, std::vector<int> bposArr, std::string pattern);
+    //implements both heuristics to search for patter in string
     std::vector<int> findIndex(const std::string& text, const std::string& pat);
-    int get_loops();
+    //returns runtime of algorithm
     double get_runTime();
 
 };
